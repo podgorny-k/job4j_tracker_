09ae09df8c27c25e0cfbe6b12556eb2a576c6deb
Konstantin Podgorny <podgorny.kv@gmail.com>
2021-02-23 23:32:34 +0300

    1.1. Тесты на StartUI. [#446632]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -149,4 +149,27 @@ public class StartUITest {
                         "1. Exit Program" + System.lineSeparator() +
                         "=== Exit Program ====" + System.lineSeparator()));
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"99", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu.%n"
+                                + "0. Exit Program%n"
+                                + "Wrong input, you can select: 0 .. 0%n"
+                                + "Menu.%n"
+                                + "0. Exit Program%n"
+                                + "=== Exit Program ====%n"
+                )
+        ));
+    }
 }
